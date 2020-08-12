e3a933d377fd8366b7e3eccf07bded8772f2807f
Cherevko88 <vladimirovich0705@gmail.com>
2020-08-13 02:12:52 +0300

    add models Catalog, Products, Haircut

--- barbershop/products/admin.py
+++ barbershop/products/admin.py
@@ -1,12 +1,30 @@
 from django.contrib import admin
-from .models import Products
+from .models import Haircut, Catalog, Products
 
 
-class ProductsAdmin(admin.ModelAdmin):
+class HaircutAdmin(admin.ModelAdmin):
     fieldsets = [
         ("None", {'fields': ['name', 'prices', 'description']})
     ]
     list_display = ('name', 'prices', 'description')
 
 
-admin.site.register(Products, ProductsAdmin)
+admin.site.register(Haircut, HaircutAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Catalog, CategoryAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'price', 'stock', 'available', 'created', 'updated')
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'stock', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Products, ProductAdmin)
