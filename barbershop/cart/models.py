6b512e98f988d35db4a8225284571d74908326b9
colliderBang <collide.ua@gmail.com>
2020-08-16 01:08:08 +0300

    WIP

--- barbershop/cart/models.py
+++ barbershop/cart/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Cart(models.Model):
     # TODO Wait products app
-    # cart_product_id = models.ForeignKey(Products, on_delete=models.CASCADE)
-    cart_count = models.PositiveSmallIntegerField(default=1)
+    user = models.ForeignKey(User, )
     cart_pub_date = models.DateTimeField('date published')
+
+class CartItem(models.Model):
