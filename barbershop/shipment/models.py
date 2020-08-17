b2174e7920b33bc9c8df894730d903ebad3e79b7
ymoskovchuk <y.a.moskovchuk@gmail.com>
2020-08-17 21:41:33 +0300

    shipment model

--- barbershop/shipment/models.py
+++ barbershop/shipment/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 
 class Shipment(models.Model):
-    description = models.CharField(max_length=128, blank=True)
-    cart = models.ForeignKey('user.cart', on_delete=models.CASCADE)
+    method = models.CharField(max_length=30, default="novaposhta")
+    description = models.CharField(max_length=100)
+    #cart = models.ForeignKey('Cart', on_delete=models.CASCADE)
     buyer = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.method
+
