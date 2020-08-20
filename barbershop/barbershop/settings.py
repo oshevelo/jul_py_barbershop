f9a9b3f9616e4b3395e6d48dc7e68c4f119c4c28
Alex shevelo <oshevelo@softserveinc.com>
2020-08-20 20:52:38 +0300

    fix

--- barbershop/barbershop/settings.py
+++ barbershop/barbershop/settings.py
@@ -94,8 +94,12 @@ WSGI_APPLICATION = 'barbershop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
@@ -145,3 +149,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static/'
+
+from local  import *
+
