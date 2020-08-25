3350e47dab63997b8c19f9718d69f5fc6a0b5e1d
Cherevko88 <vladimirovich0705@gmail.com>
2020-08-25 23:58:08 +0300

    test push

--- barbershop/barbershop/settings.py
+++ barbershop/barbershop/settings.py
@@ -34,10 +34,8 @@ INSTALLED_APPS = [
     'payments',
     'products.apps.ProductsConfig',
     'cart',
-
     'jet',
     'rest_framework',
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -83,8 +81,12 @@ WSGI_APPLICATION = 'barbershop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'barbershop',
+        'USER': 'barbershop',
+        'PASSWORD': 'barbershop',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
