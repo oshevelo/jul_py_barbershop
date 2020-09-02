40bbc81aebedeb71bfa68ca6058bbe1e76aae570
Yurii-Savchuk <savchuckyura@gmail.com>
2020-09-02 20:22:59 +0300

    bew wp + add WhoDidIt

--- barbershop/worker_profile/admin.py
+++ barbershop/worker_profile/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import WorkerProfile
+from .models import WorkerProfile, WorkerCommunications
+
 
 # Register your models here.
 
@@ -8,8 +9,16 @@ class WorkerProfileAdmin(admin.ModelAdmin):
     fieldsets = [
         ('None', {'fields': ['first_name', 'second_name', 'position', 'phone_number', 'email']})
     ]
-    list_display = ('first_name', 'second_name', 'position', 'phone_number', 'email', 'type')
-    list_filter = ['position']
+    list_display = ('first_name', 'second_name', 'position', 'phone_number', 'email', 'created_on', 'updated_by',)
+    list_filter = ['position', 'created_on', 'updated_by']
 
 
 admin.site.register(WorkerProfile, WorkerProfileAdmin)
+
+
+class WorkerCommunicationsAdmin(admin.ModelAdmin):
+    list_display = ('worker_profile', 'worker', 'type', 'created_on', 'updated_by')
+    list_filter = ['created_on', 'updated_by']
+
+
+admin.site.register(WorkerCommunications, WorkerCommunicationsAdmin)
