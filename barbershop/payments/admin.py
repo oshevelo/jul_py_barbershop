5d9bb2aa5ae845b931a88adaa7f117fae73134ea
maria <maria.shnaider@gmail.com>
2020-09-17 14:35:42 +0300

    fix models

--- barbershop/payments/admin.py
+++ barbershop/payments/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Payment
 
-# Register your models here.
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('id',
+                    #               'order',
+                    'bill_amount',
+                    'type',
+                    'status',
+                    'payee_name',
+                    'payee_surname',
+                    'payee_email',
+                    'user',
+                    'created_on',
+                    'updated_on',
+                    'created_by',
+                    'updated_by',
+                    )
+
+
+admin.site.register(Payment, PaymentAdmin)
--- barbershop/payments/models.py
+++ barbershop/payments/models.py
@@ -1,17 +1,75 @@
 from django.db import models
-#from cart.models import Order
+from apps_generic.whodidit.models import WhoDidIt
 
 
-class Payment(models.Model):
-    #amount = models.OneToOneField(Order, on_delete=models.CASCADE)
-    description = models.CharField(max_length=128, blank=True)
-    buyer = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    paid = models.BooleanField(default=False)
+class Payment(WhoDidIt):
+    # order = models.ForeignKey('orders.Order', on_delete=models.PROTECT, null=True, blank=False)
+    bill_amount = models.DecimalField(max_digits=10, decimal_places=2)
 
-    def __str__(self):
-        return 'Payment {} is {}'.format(self.id, self.paid)
-    
-        
-        
+    class PaymentType:
+        cash = 'cash'
+        portmone = 'portmone'
+
+    TYPE = [
+        (PaymentType.cash, 'cash'),
+        (PaymentType.portmone, 'Portmone'),
+    ]
+
+    type = models.CharField(
+        max_length=20,
+        choices=TYPE,
+        default=PaymentType.cash,
+    )
+
+    class PaymentStatus:
+        waiting = 'waiting'
+        input = 'input'
+        preauth = 'preauth'
+        confirmed = 'confirmed'
+        rejected = 'rejected'
+        refunded = 'refunded'
+        error = 'error'
+
+    STATUS = [
+        (PaymentStatus.waiting, 'waiting'),
+        (PaymentStatus.input, 'input'),
+        (PaymentStatus.preauth, 'preauth'),
+        (PaymentStatus.confirmed, 'confirmed'),
+        (PaymentStatus.rejected, 'rejected'),
+        (PaymentStatus.refunded, 'refunded'),
+        (PaymentStatus.error, 'error'),
+    ]
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS,
+        default=PaymentStatus.waiting,
+    )
+
+    payee_name = models.CharField(max_length=50)
+    payee_surname = models.CharField(max_length=50)
+    payee_email = models.CharField(max_length=50)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Payment'
+        verbose_name_plural = 'Payments'
+
+    # def __str__(self):
+    #    return 'payment for order {} by {} is {}'.format(self.order, self.user, self.status)
+
+
+class PaymentSystemLog(models.Model):
+    # request
+    # shop_order_number = models.ForeignKey('orders.Order', on_delete=models.PROTECT, null=True, blank=False)
+    payee_id = models.IntegerField(null=True, blank=False)
+    dt = models.DateTimeField(auto_now=True)
+    bill_amount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=False)
+    # response
+    SHOPBILLID = models.IntegerField(null=True, blank=False)
+    SHOPORDERNUMBER = models.CharField(max_length=100, null=True, blank=False)
+    BILL_AMOUNT = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=False)
+    RESULT = models.IntegerField(null=True, blank=False)
+
+    # def __str__(self):
+    #    return 'order {} by {} is {}'.format(self.shop_order_number, self.payee_id, self.RESULT)
