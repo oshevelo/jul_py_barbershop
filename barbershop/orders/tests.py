e7073cccd752ce7ba9389145ef816d263c7c172b
Alex shevelo <oshevelo@softserveinc.com>
2020-10-04 14:57:06 +0300

    test

--- barbershop/orders/tests.py
+++ barbershop/orders/tests.py
@@ -27,7 +27,7 @@ class OrdersListAPITest(TestCase):
 
     def test_orders_list(self):
 
-        self.c.login(username='tt', password='111')
+        self.c.login(username='tt', password='111')gi
         response = self.c.get(
             '/orders/'
         )
