90316574f8ce9f4a82e3de393a421a982ca31163
Cherevko88 <vladimirovich0705@gmail.com>
2020-10-08 20:02:46 +0300

    qwer

--- barbershop/products/tests.py
+++ barbershop/products/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
 
-# Create your tests here.
+
+class ProductsListTest(TestCase):
+    def setUp(self):
+        self.c = APIClient()
+
+    def test_products_list(self):
+        response = self.c.get(
+            '/products/catalog/products/'
+        )
+        print(response.status_code)
+        print(response.data)
+        self.assertEqual(True, True)
+
