863db98e0ca307dc10936be74bcda0a233b86337
Yurii-Savchuk <savchuckyura@gmail.com>
2020-10-09 21:24:09 +0300

    marge with master + fix wp permissions

--- barbershop/worker_profile/permissions.py
+++ barbershop/worker_profile/permissions.py
@@ -3,8 +3,6 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class WorkerPermission(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS and obj.worker_profile.worker == request.user:
-            return obj.worker_profile.worker
-        elif request.method in SAFE_METHODS and request.worker.position == 'admin':
+        if request.worker.position == 'admin':
             return True
-
+        return obj.worker_profile.worker == request.user
